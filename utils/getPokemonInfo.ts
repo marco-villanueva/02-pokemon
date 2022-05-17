6131196c1d4f81e171ca81cdb6541343e3233ad0
Marco Villanueva <marco.villanueva@globant.com>
2022-05-17 13:51:01 -0500

    añadiendo funcionalidad de busqueda (rehidratacion dinamica)

--- utils/getPokemonInfo.ts
+++ utils/getPokemonInfo.ts
@@ -4,14 +4,19 @@ import { PokemonDetails } from "../interfaces";
 
 export const getPokemonInfo = async (nameOrId: string) => {
 
-    const { data } = await pokeApi.get<PokemonDetails>(
-        `/pokemon/${nameOrId}`
-    );
+    try {
+        
+        const { data } = await pokeApi.get<PokemonDetails>(
+            `/pokemon/${nameOrId}`
+        );
+    
+        return {
+            id: data.id,
+            name: data.name,
+            sprites: data.sprites
+        }
 
-    return {
-        id: data.id,
-        name: data.name,
-        sprites: data.sprites
+    } catch (error) {
+        return null;        
     }
-
 }
